7249222b44312a8fb151ae868fe38b013bc3a051
bajahaw <rathi7137@gmail.com>
2025-01-18 23:03:14 +0100

    fixed mobile view and added new About me section

--- src/main/java/tech/radhi/portfolio/Controller.java
+++ src/main/java/tech/radhi/portfolio/Controller.java
@@ -6,11 +6,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @org.springframework.stereotype.Controller
 public class Controller {
     @ResponseBody
-    @GetMapping("/ai")
-    public String response(){
+    @GetMapping("/overview")
+    public String overview(){
         return """
                 Hi there! Radhi is a software
                         developer who loves diving into web development and backend projects, especially with Java. When he is not
+                        coding, you’ll probably find me strategizing over a game of chess. he is also a computer science major and a
+                        proud Arch Linux user, always up for exploring new ideas and technologies . . .
+                   \s""";
+    }
+
+    @ResponseBody
+    @GetMapping("/about-me-brief")
+    public String aboutMeBrief(){
+        return """
+                Radhi is a software
+                        engineering student who loves diving into web development and backend projects, especially with Java. When he is not
                         coding, you’ll probably find me strategizing over a game of chess. he is also a computer science major and a
                         proud Arch Linux user, always up for exploring new ideas and technologies . . .
                    \s""";

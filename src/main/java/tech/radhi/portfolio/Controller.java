55091e8679e1ff6f2444f752b4aff4e1f742d037
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-03-15 01:35:22 +0100

    added template for about page

--- src/main/java/tech/radhi/portfolio/Controller.java
+++ src/main/java/tech/radhi/portfolio/Controller.java
@@ -9,32 +9,37 @@ public class Controller {
     @GetMapping("/overview")
     public String overview(){
         return """
-                Hi there! Radhi is a software
-                        developer who loves diving into web development and backend projects, especially with Java. When he is not
-                        coding, you’ll probably find me strategizing over a game of chess. he is also a computer science major and a
-                        proud Arch Linux user, always up for exploring new ideas and technologies . . .
-                   \s""";
+            Hi there! Radhi is a software
+                    developer who loves diving into web development and backend projects, especially with Java. When he is not
+                    coding, you’ll probably find me strategizing over a game of chess. he is also a computer science major and a
+                    proud Arch Linux user, always up for exploring new ideas and technologies . . .
+               \s""";
     }
 
     @ResponseBody
     @GetMapping("/about-me-brief")
     public String aboutMeBrief(){
         return """
-                Radhi currently is a computer science
-                        engineering student at the University of Dunaujvaros - Hungary. He is passionate about technology and loves to 
-                        explore new ideas and concepts. Radhi is a quick learner and enjoys working on challenging projects that push him
-                        to think outside the box. He is always looking for new opportunities to grow and develop his skills . . .
-                   \s""";
+            Radhi currently is a computer science
+                    engineering student at the University of Dunaujvaros - Hungary. He is passionate about technology and loves to 
+                    explore new ideas and concepts. Radhi is a quick learner and enjoys working on challenging projects that push him
+                    to think outside the box. He is always looking for new opportunities to grow and develop his skills . . .
+               \s""";
     }
 
     @ResponseBody
     @GetMapping("/social-overview")
     public String socialOverview(){
         return """
-                When Radhi's not coding, he's a dynamic online presence, sparking conversations and connections.
-                        With a knack for fostering meaningful relationships, he's a magnet for like-minded individuals who
-                        share his passion for innovation. Radhi's online persona is a reflection of his curious and
-                        adventurous spirit, always seeking out new perspectives and ideas.
-                   \s""";
+            When Radhi's not coding, he's a dynamic online presence, sparking conversations and connections.
+                    With a knack for fostering meaningful relationships, he's a magnet for like-minded individuals who
+                    share his passion for innovation. Radhi's online persona is a reflection of his curious and
+                    adventurous spirit, always seeking out new perspectives and ideas.
+               \s""";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+            return "about";
     }
 }

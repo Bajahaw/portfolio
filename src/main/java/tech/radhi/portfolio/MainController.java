a926b91cdb29c0f6640d947c390eb8d9961c1b09
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-03-17 07:45:39 +0100

    fix q-a template

--- src/main/java/tech/radhi/portfolio/MainController.java
+++ src/main/java/tech/radhi/portfolio/MainController.java
@@ -1,10 +1,14 @@
 package tech.radhi.portfolio;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+import java.util.List;
+
+@Controller
+public class MainController {
     @ResponseBody
     @GetMapping("/overview")
     public String overview(){
@@ -21,7 +25,7 @@ public class Controller {
     public String aboutMeBrief(){
         return """
             Radhi currently is a computer science
-                    engineering student at the University of Dunaujvaros - Hungary. He is passionate about technology and loves to 
+                    engineering student at the University of Dunaujvaros - Hungary. He is passionate about technology and loves to
                     explore new ideas and concepts. Radhi is a quick learner and enjoys working on challenging projects that push him
                     to think outside the box. He is always looking for new opportunities to grow and develop his skills . . .
                \s""";
@@ -39,7 +43,15 @@ public class Controller {
     }
 
     @GetMapping("/about")
-    public String about(){
-            return "about";
+    public String about(Model model){
+        model.addAttribute("questions",
+                List.of(
+                        new QaItem("first", "who am I?"),
+                        new QaItem("second", "What is my career goal?"),
+                        new QaItem("third", "What tech stack I use?"),
+                        new QaItem("forth", "What projects did I do?")
+                ));
+        return "about";
     }
 }
+

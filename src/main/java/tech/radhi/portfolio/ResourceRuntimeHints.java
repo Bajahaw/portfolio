523570ae6ac949244ca79461280a2dcf22796d08
Radhi <153525225+Bajahaw@users.noreply.github.com>
2025-05-18 18:24:51 +0200

    fix: added new jte classes to reflection Runtime hints

--- src/main/java/tech/radhi/portfolio/ResourceRuntimeHints.java
+++ src/main/java/tech/radhi/portfolio/ResourceRuntimeHints.java
@@ -4,6 +4,8 @@ import gg.jte.generated.precompiled.JteaboutGenerated;
 import gg.jte.generated.precompiled.JteindexGenerated;
 import gg.jte.generated.precompiled.JtemainGenerated;
 import gg.jte.generated.precompiled.JteqaitemGenerated;
+import gg.jte.generated.precompiled.fragments.Jte403Generated;
+import gg.jte.generated.precompiled.fragments.Jte404Generated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.MemberCategory;
@@ -11,6 +13,8 @@ import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.stereotype.Component;
 import tech.radhi.portfolio.content.ProjectTemplate;
+
+import java.util.List;
 
 @Component
 public class ResourceRuntimeHints implements RuntimeHintsRegistrar {
@@ -22,33 +26,22 @@ public class ResourceRuntimeHints implements RuntimeHintsRegistrar {
 
         log.info("Registering hints for resources");
 
-        hints.resources()
-                .registerPattern("**/*.bin");
+        var classes = List.of(
+                JteindexGenerated.class,
+                JteaboutGenerated.class,
+                JtemainGenerated.class,
+                JteqaitemGenerated.class,
+                ProjectTemplate.class,
+                Jte403Generated.class,
+                Jte404Generated.class
+        );
 
-        hints.reflection()
-                .registerType(JteindexGenerated.class,
-                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        MemberCategory.INVOKE_DECLARED_METHODS
-                );
-        hints.reflection()
-                .registerType(JteaboutGenerated.class,
-                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        MemberCategory.INVOKE_DECLARED_METHODS
-                );
-        hints.reflection()
-                .registerType(JtemainGenerated.class,
-                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        MemberCategory.INVOKE_DECLARED_METHODS
-                );
-        hints.reflection()
-                .registerType(JteqaitemGenerated.class,
-                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        MemberCategory.INVOKE_DECLARED_METHODS
-                );
-        hints.reflection()
-                .registerType(ProjectTemplate.class,
-                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        MemberCategory.INVOKE_DECLARED_METHODS
-                );
+        classes.forEach(c ->
+                hints.reflection().registerType(c,
+                            MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                            MemberCategory.INVOKE_DECLARED_METHODS
+                )
+        );
+        hints.resources().registerPattern("**/*.bin");
     }
 }
